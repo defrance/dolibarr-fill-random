7682d5e2d4cb6fa1fc3a030014e37f5f0b033d58
AlexyCatelle <alexy.catelle@oclock.school>
2025-11-03 14:33:58 +0100

    get random project et fill projects

---
+++ utils/get_random/get_random_project.py
@@ -0,0 +1,17 @@
+import random
+
+import os, sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
+from utils.fill_data.fill_projects import fill_projects
+
+def get_random_projectID(retDataProjects):
+	if (len(retDataProjects) > 1):
+		return retDataProjects[random.randint(1, len(retDataProjects)-1)]['id']
+	elif (len(retDataProjects) == 1):
+		return retDataProjects[0]['id']
+	return 0
+
+# Test unitaire
+if __name__ == "__main__":
+    retDataProjects = fill_projects(10)
+    print(get_random_projectID(retDataProjects))
