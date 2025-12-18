949f49513fc96c0668456a9b04a1b2c1b925dea1
AlexyCatelle <alexy.catelle@oclock.school>
2025-12-18 17:25:37 +0100

    lot mouvement de stock

---
+++ utils/get_enabled_module.py
@@ -0,0 +1,23 @@
+import requests
+import sys, os
+
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
+from dolibarr_api import *
+
+
+
+def get_enabled_modules():
+	# l'url correspond à l'adresse de du site ainsi que le chemin de l'api
+	url = urlBase + "setup/modules"
+	rEnabledModules = requests.get(url, headers=headers, verify=False)
+	if rEnabledModules.status_code != 200:
+		print('Erreur lors de la récupération des modules', rEnabledModules.status_code)
+		print (rEnabledModules.text)
+		exit()
+
+	retDataEnabledModules = rEnabledModules.json()
+	return retDataEnabledModules
+
+if __name__ == "__main__":
+	print(get_enabled_modules())
