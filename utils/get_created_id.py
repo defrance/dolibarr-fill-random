84dd36cc5e0e95d07602be114ec312649a8f9d6a
AlexyCatelle <alexy.catelle@oclock.school>
2026-01-14 11:42:37 +0100

    correction merge

--- utils/get_created_id.py
+++
@@ -1,6 +0,0 @@
-def get_created_id(response):
-    try:
-        data = response.json()
-        return data["id"] if isinstance(data, dict) else int(data)
-    except Exception:
-        return int(response.text.strip())
